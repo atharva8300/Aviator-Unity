c016ddc5eb738d5c414db43d40dab16d51e87c55
buddy8300 <atharva8300@gmail.com>
2020-04-19 08:51:22 +0530

    Added level3

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
-    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float rcsThrust = 100f;//rotation 
     [SerializeField] float mainThrust = 50f;
     [SerializeField] float levelLoadDelay = 2f;
     
@@ -18,8 +19,10 @@ public class Rocket : MonoBehaviour
     Rigidbody rigidBody;
     AudioSource audioSource;
 
-    enum State  {Alive,Dying,Transcending };
+    enum State  {Alive,Dying,Transcending}; //struct
     State state = State.Alive ;
+
+    bool collisionsDisabled = false ;
 
     // Start is called before the first frame update
     void Start()
@@ -35,14 +38,30 @@ public class Rocket : MonoBehaviour
         {
             RespondToThrustInput();
             RespondToRotateInput();
+        }
+        if(Debug.isDebugBuild)
+        {
+            RespondToDebugKeys();  // c and l debug keys
+        }
+        
+    }
+
+    private void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionsDisabled = !collisionsDisabled; // toggle between collisions
 
         }
     }
 
-
-    void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision) // jabhi bhi collision 
     {
-        if (state != State.Alive)
+        if (state != State.Alive || collisionsDisabled)
         {
             return; // ignore collisions after death 
         }
@@ -51,12 +70,12 @@ public class Rocket : MonoBehaviour
         {
 
             case "Friendly":
-                //pass
+                //pass 
                 break;
             case "Finish":
                 StartSuccessSequence();
                 break;
-            default:
+            default: // untagged
                 StartDeathSequence();
                 break;
         }
@@ -116,7 +135,7 @@ public class Rocket : MonoBehaviour
         {
             audioSource.PlayOneShot(mainEngine);
         }
-        mainEngineParticles.Play();
+        mainEngineParticles.Play(); //flames
     }
 
     private void RespondToRotateInput()
@@ -124,11 +143,11 @@ public class Rocket : MonoBehaviour
         rigidBody.freezeRotation = true; // take manual control of rotation 
 
         
-        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        float rotationThisFrame = rcsThrust * Time.deltaTime ;
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward * rotationThisFrame);
+            transform.Rotate(Vector3.forward * rotationThisFrame); //anti 
         }
         else if (Input.GetKey(KeyCode.D)) //else if used as rocket shouldn't rotate on bs
         {
